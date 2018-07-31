5c06086bbf8ec6c9e5e42d47710dc590689c37d2
Guy Ben-Yosef <gby@victoria.csail.mit.edu>
2018-07-30 22:18:15 -0400

    CSAIL

--- useful_cmds.sh
+++ useful_cmds.sh
@@ -1,4 +1,5 @@
-# run their best model: e.g., 
+#!/usr/bin/env bash
+# run their best model: e.g.,
 python main.py --input ./input --video_root ./../../Interactions/data/ --output ./output.json --model /storage/gby/models/C3Ds/resnext-101-kinetics.pth --resnet_shortcut B --mode score --batch_size 6 --model_depth 101 --model_name resnext
 
 # generate tags: e.g.,
@@ -6,3 +7,12 @@ python generate_result_video/generate_result_video.py ./output.json ../../Intera
 
 # generating gif: e.g.,
 ffmpeg -i _zazCBo7Ppk_000010_000020.mp4 -framerate 5 _zazCBo7Ppk_000010_000020.gif
+
+# generate GIFs for a list of MP4: e.g., 
+sh generate_gif_for_mp4.sh ../input videos_pred/
+
+
+
+
+
+ffmpeg -i D9uDROiAEig_000040_000050.mp4 -framerate 5 D9uDROiAEig_000040_000050.gif
